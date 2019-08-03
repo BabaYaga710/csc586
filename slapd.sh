4a263b10506bd3da114a8e1aaab554f5e29a63dd
BabaYaga710 <43589411+BabaYaga710@users.noreply.github.com>
2019-08-03 03:01:37 -0400

    Update slapd.sh

--- slapd.sh
+++ slapd.sh
@@ -1,78 +1,114 @@
-#!/bin/bash
+#!/bin/bash 
+# make sure to run script as sudo 
 
-sudo apt-get update
+
+# LDAP 
+
+# update first 
+apt-get -q -y update 
+
+# install maven 
+
+apt-get install -y maven 
+
+# Install php dependencies 
+
+apt-get -y install php php-cgi libapache2-mod-php php-common php-pear php-mbstring 
+
+a2enconf php7.0-cgi 
+
+service apache2 restart 
+
+# Pre-seed the slapd passwords 
+
+export DEBIAN_FRONTEND='non-interactive'
+
+echo -e "slapd slapd/root_password password KappaRoss" |debconf-set-selections
+echo -e "slapd slapd/root_password_again password KappaRoss" |debconf-set-selections
+
+echo -e "slapd slapd/internal/adminpw password test" |debconf-set-selections
+echo -e "slapd slapd/internal/generated_adminpw password test" |debconf-set-selections
+echo -e "slapd slapd/password2 password test" |debconf-set-selections
+echo -e "slapd slapd/password1 password test" |debconf-set-selections
+echo -e "slapd slapd/domain string acu.local" |debconf-set-selections
+echo -e "slapd shared/organization string IT410" |debconf-set-selections
+echo -e "slapd slapd/backend string HDB" |debconf-set-selections
+echo -e "slapd slapd/purge_database boolean true" |debconf-set-selections
+echo -e "slapd slapd/move_old_database boolean true" |debconf-set-selections
+echo -e "slapd slapd/allow_ldap_v2 boolean false" |debconf-set-selections
+echo -e "slapd slapd/no_configuration boolean false" |debconf-set-selections
+
+# Grab slapd and ldap-utils (pre-seeded)
+apt-get install -y slapd ldap-utils phpldapadmin
+
+# Must reconfigure slapd for it to work properly 
+sudo dpkg-reconfigure slapd 
+
+# Gotta replace the ldap.conf file, it comments out stuff we need set by default - first open it for writing 
+
+chmod 777 /etc/ldap/ldap.conf 
+
+cat <<'EOF' > /etc/ldap/ldap.conf
+#
+# LDAP Defaults
+#
+
+# See ldap.conf(5) for details
+# This file should be world readable but not world writable.
 
 
-set -eu
+BASE    dc=acu,dc=local
+URI     ldap://104.219.54.109 ldap://104.219.54.109:666
 
-status () {
-  echo "---> ${@}" >&2
-}
+#SIZELIMIT      12
+#TIMELIMIT      15
+#DEREF          never
 
-set +x
-: LDAP_ROOTPASS=password
-: LDAP_DOMAIN=clemson.cloudlab.us
-: LDAP_ORGANISATION=clemson.cloudlab.us
-
-  cat <<EOF | debconf-set-selections
-slapd slapd/password1 password password
-slapd slapd/password2 password password
-slapd shared/organization string clemson.cloudlab.us
-slapd slapd/move_old_database boolean true
-slapd slapd/dump_database_destdir string /var/backups/slapd-VERSION
-slapd slapd/upgrade_slapcat_failure error
-slapd slapd/invalid_config boolean true
-slapd slapd/backend select MDB
-slapd slapd/purge_database boolean false
-slapd slapd/ppolicy_schema_needs_update select abort installation
-slapd slapd/dump_database select when needed
-slapd slapd/no_configuration boolean false
-slapd slapd/unsafe_selfwrite_acl note
-slapd slapd/domain string clemson.cloudlab.us
-slapd slapd/allow_ldap_v2 boolean false
+# TLS certificates (needed for GnuTLS)
+TLS_CACERT      /etc/ssl/certs/ca-certificates.crt
 EOF
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y slapd ldap-utils
-
-DEBIAN_FRONTEND=noninteractive dpkg-reconfigure slapd
-
-  cat <<EOF > basedn.ldif
-dn: ou=People,dc=clemson,dc=cloudlab,dc=us
-objectClass: organizationalUnit
-ou: People
-
-dn: ou=Groups,dc=clemson,dc=cloudlab,dc=us
-objectClass: organizationalUnit
-ou: Groups
-
-dn: cn=CSC,ou=Groups,dc=clemson,dc=cloudlab,dc=us
-objectClass: posixGroup
-cn: CSC586
-gidNumber: 5000
-EOF
+# Be safe again 
+chmod 744 /etc/ldap/ldap.conf 
 
 
+# Now change all values in /etc/phpldapadmin/config.php to their actual values from example, or .com or localhost (I use sed)
 
-  cat <<EOF > users.ldif
-dn: uid=student,ou=People,dc=clemson,dc=cloudlab,dc=us
-objectClass: inetOrgPerson
-objectClass: posixAccount
-objectClass: shadowAccount
-uid: student
-sn: Ram
-givenName: Golden
-cn: student
-displayName: student
-uidNumber: 10000
-gidNumber: 5000
-userPassword: password
-gecos: Golden Ram
-loginShell: /bin/dash
-homeDirectory: /home/student
-EOF
 
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -f basedn.ldif
+# Line 286 
+sed -i "s@$servers->setValue('server','name','My LDAP Server');.*@$servers->setValue('server','name','Nathans_LDAP');@" /etc/phpldapadmin/config.php
+# Line 293 
+sed -i "s@$servers->setValue('server','host','127.0.0.1');.*@$servers->setValue('server','host','104.219.54.109');@" /etc/phpldapadmin/config.php 
+# Line 300 
+sed -i "s@$servers->setValue('server','base',array('dc=example,dc=com'));.*@$servers->setValue('server','base',array('dc=acu,dc=local'));@" /etc/phpldapadmin/config.php
+# Line 326 
+sed -i "s@$servers->setValue('login','bind_id','cn=admin,dc=example,dc=com');.*@$servers->setValue('login','bind_id','cn=admin,dc=acu,dc=local');@" /etc/phpldapadmin/config.php
 
-ldapadd -x -D cn=admin,dc=clemson,dc=cloudlab,dc=us -W -f users.ldif
+# Prevent error when creating users 
 
-ldapsearch -x -LLL -b dc=clemson,dc=cloudlab,dc=us 'uid=student' cn gidNumber
+sed -i "s@$default = $this->getServer()->getValue('appearance','password_hash');.*@$default = $this->getServer()->getValue('appearance','password_hash_custom');@g" /usr/share/phpldapadmin/lib/TemplateRender.php
+
+service apache2 restart 
+
+echo ------------------------# 
+echo 'PHPldapadmin installed.'
+echo ------------------------# 
+
+echo ""
+
+echo ------------------------------------------------------------# 
+echo 'Can now access phpldapadmin at http://your-ip/phpldapadmin.'
+echo ------------------------------------------------------------# 
+
+echo ""
+
+echo ------------------------------------------------------------------------#
+echo 'Username should be acu.local, password is the adminpw set during setup.'
+echo ------------------------------------------------------------------------# S
+
+# Logging 
+
+echo -e 'Maven installed -done by' $USER 'at time\n' $DATE '\n' >> /var/log/installs/log.txt
+echo -e 'slapd and ldap-utils configured and installed -done by' $USER 'at time\n' $DATE '\n' >> /var/log/installs/log.txt
+echo -e 'phpldapadmin install configured -done by' $USER 'at time\n' $DATE '\n' >> /var/log/installs/log.txt
+echo -e 'LDAP installed completed by' $USER 'at time\n' $DATE '\n' >> /var/log/installs/log.txt
